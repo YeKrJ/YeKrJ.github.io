f6caab2a54c7c96f838dc58c7e91574c7d43a5f8
YeKrJ <newjuni@kakao.com>
2022-12-05 22:58:35 +0900

    Update shufflepage.js

--- shufflepage.js
+++ shufflepage.js
@@ -62,9 +62,25 @@ function shufflePage() {
     if (j > 70) {
       if (modeMulti) {
     for(let i=1; i<=6; i++) {
+      noStroke()
+      fill(120);
+      rect(104*wX, 151.5*wX, 180*wX, 29.5*wX, 2*wX);
+      rect(104*wX, 191.5*wX, 180*wX, 29.5*wX, 2*wX);
+      textFont(fontgm, 8*wX);
+      textAlign(CENTER);
+      text("순서대로 옵션을 입력하세요", 193*wX, 142*wX);
+      textSize(6*wX)
+      text("적은 옵션 수와 선택한 카드 수를 맞춰주세요", 193*wX, 55*wX);
       inp[i].inp.show();
     }
   } if (modeTwo) {
+    noStroke()
+    fill(120);
+    rect(139*wX, 151.5*wX, 111*wX, 29.5*wX, 2*wX);
+    rect(139*wX, 191.5*wX, 111*wX, 29.5*wX, 2*wX);
+    textFont(fontgm, 8*wX);
+    textAlign(CENTER);
+    text("이름을 입력하세요", 193*wX, 142*wX);
     inp[7].inp.show();
     inp[8].inp.show();
   }
